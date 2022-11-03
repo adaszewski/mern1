38b5fa747d78c3cf0e7842751330432506017326
Robert Adaszewski <adaszewski@protonmail.com>
2022-11-03 08:16:07 +0100

    update i delete z błedem update

--- app/controllers/event.controller.js
+++ app/controllers/event.controller.js
@@ -1,28 +1,63 @@
-const Event = require('../models/Event')
+const Event = require("../models/Event");
 
-function eventTable(cb){
-    Event.find().lean().exec(function(err, events) {
-        if(err) {
-            cb(err)
-        } else {
-            cb(null, events)
-        }   
-    });
-}
-function eventAdd(data,cb){
-    let newEvent=new Event(data)
-
-    newEvent.save(function(err, event) {
-        if(err) {
-            cb(err)
-        } else {
-            cb(null, event)
-        }   
+function eventTable(cb) {
+  Event.find()
+    .lean()
+    .exec(function (err, events) {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, events);
+      }
     });
 }
 
-
-    module.exports = {
-        list: eventTable,
-        add: eventAdd
+function eventGet(_id, cb) {
+  Event.findById(_id).exec(function (err, event) {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, event);
     }
+  });
+}
+
+function eventAdd(data, cb) {
+  let newEvent = new Event(data);
+
+  newEvent.save(function (err, event) {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, event);
+    }
+  });
+}
+
+function eventUpdate(id, data, cb) {
+  Event.updateOne({ id: id }, data, function (err, event) {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, event);
+    }
+  });
+}
+
+function eventDelete(id, cb) {
+  Event.deleteOne({ _id: id }, function (err, event) {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, event);
+    }
+  });
+}
+
+module.exports = {
+  list: eventTable,
+  get: eventGet,
+  add: eventAdd,
+  update: eventUpdate,
+  delete: eventDelete,
+};
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const hbs = require("express-handlebars");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.engine("hbs", hbs.engine({ defaultLayout: "main", extname: ".hbs" }));
@@ -16,8 +16,7 @@ const event = require("./app/controllers/event.controller");
 app.get("/", (req, res) => {
   res.render("home", {
     title: "Zapisz się na kurs",
-    content: "formularz",
-    
+    content: "http://localhost:3000/event",
   });
 });
 
@@ -27,7 +26,6 @@ app.get("/mongoose", (req, res) => {
     .exec(function (err, events) {
       console.log(events);
     });
-
 
   newEvent.save(function (err) {
     console.log("zrobione");
@@ -43,26 +41,50 @@ app.get("/event", function (req, res) {
   });
 });
 
-app.get('/json', function(req, res){
-  event.list(function(err, events){
-      if(err) {
-          res.status(404);
-          res.json({
-              error: "brak wydarzeń"
-          });
-      } else {
-          res.json(events);
-      }
+app.get("/event/:id", function (req, res) {
+  event.get(req.params.id, function (err, event) {
+    if (err) res.send(err);
+    res.render("mod_event", event)
   });
 });
 
+app.get("/json", function (req, res) {
+  event.list(function (err, events) {
+    if (err) {
+      res.status(404);
+      res.json({
+        error: "brak wydarzeń",
+      });
+    } else {
+      res.json(events);
+    }
+  });
+});
 
 app.post("/add", function (req, res) {
-  event.add(req.body, function(err){
-    if(err) res.send(err);
-    res.redirect("/event")
-  })
+  event.add(req.body, function (err) {
+    if (err) res.send(err);
+    res.redirect("/event");
+  });
 });
+
+app.get('/event/del/:id', function(req, res){
+     
+  event.delete(req.params.id, function(err, post){
+      if(err) res.send(err);
+      res.redirect("/event");
+    });
+   
+  })
+app.post('/update/:id', function(req, res){
+     
+  event.update(req.params.id, req.body, function(err, event){
+      if(err) res.send(err);
+      res.redirect("/event");
+  });
+   
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
